3bccdf45b2534d97e4401e3de4fe0e333e79e24e
AndreLopes60000 <113781649+AndreLopes60000@users.noreply.github.com>
2024-04-16 16:42:28 +0100

    Added Discovery and Result classes

--- src/tukano/api/rest/RestUsers.java
+++ src/tukano/api/rest/RestUsers.java
@@ -23,31 +23,71 @@ public interface RestUsers {
 	String QUERY = "query";
 	String USER_ID = "userId";
 	
+	/**
+	 * Creates a new user.
+	 * @param user - User to be created
+	 * @return OK - the userId of the user. 
+	 * 		CONFLICT - if the name already exists. 
+	 * 		BAD_REQUEST - otherwise.
+	 */
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	String createUser(User user);
 	
-	
+	/**
+	 * Obtains the information on the user identified by userId
+	 * @param userId - the userId of the user
+	 * @param pwd  - the password of the user
+	 * @return OK and the user object, if the userId exists and password matches the existing password; 
+	 *         FORBIDDEN - if the password is incorrect; 
+	 *         NOT_FOUND - if no user exists with the provided userId
+	 */
 	@GET
 	@Path("/{" + USER_ID+ "}")
 	@Produces(MediaType.APPLICATION_JSON)
 	User getUser(@PathParam(USER_ID) String userId, @QueryParam( PWD ) String pwd);
 	
-	
+	/**
+	 * Modifies the information of a user. Value of null, in any field of the user argument, means the field will remain as unchanged 
+	 * (the userId cannot be modified).
+	 * @param userId - the userId of the user
+	 * @param pwd  - password of the user
+	 * @param user - Updated information
+	 * @return OK and the updated user object, if the userId exists and password matches the existing password 
+	 *         FORBIDDEN - if the password is incorrect 
+	 *         NOT_FOUND - if no user exists with the provided userId 
+	 *         BAD_REQUEST - otherwise.
+	 */
 	@PUT
 	@Path("/{" + USER_ID+ "}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	User updateUser(@PathParam( USER_ID ) String userId, @QueryParam( PWD ) String pwd, User user);
 	
-	
+	/**
+	 * Deletes the user identified by userId
+	 * @param userId - the userId of the user
+	 * @param pwd - password of the user
+	 * @return OK and the deleted user object, if the name exists and pwd matches the
+	 *         existing password 
+	 *         FORBIDDEN if the password is incorrect 
+	 *         NOT_FOUND if no user exists with the provided name
+	 *         BAD_REQUEST otherwise
+	 */
 	@DELETE
 	@Path("/{" + USER_ID+ "}")
 	@Produces(MediaType.APPLICATION_JSON)
 	User deleteUser(@PathParam(USER_ID) String userId, @QueryParam(PWD) String pwd);
 	
-	
+	/**
+	 * Returns the list of users for which the pattern is a substring of the userId, case-insensitive. 
+	 * The password of the users returned by the query must be set to the empty string "".
+	 * 
+	 * @param pattern - substring to search
+	 * @return OK when the search was successful, regardless of the number of hits (including 0 hits). 
+	 *         BAD_REQUEST - otherwise.
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	List<User> searchUsers(@QueryParam(QUERY) String pattern);	
---
+++ src/tukano/helpers/Discovery.java
@@ -0,0 +1,162 @@
+package tukano.helpers;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.URI;
+import java.util.logging.Logger;
+import java.util.*;
+
+
+/**
+ * <p>A class interface to perform service discovery based on periodic 
+ * announcements over multicast communication.</p>
+ * 
+ */
+
+public interface Discovery {
+
+	/**
+	 * Used to announce the URI of the given service name.
+	 * @param serviceName - the name of the service
+	 * @param serviceURI - the uri of the service
+	 */
+	public void announce(String serviceName, String serviceURI);
+
+	/**
+	 * Get discovered URIs for a given service name
+	 * @param serviceName - name of the service
+	 * @param minReplies - minimum number of requested URIs. Blocks until the number is satisfied.
+	 * @return array with the discovered URIs for the given service name.
+	 */
+	public URI[] knownUrisOf(String serviceName, int minReplies);
+
+	/**
+	 * Get the instance of the Discovery service
+	 * @return the singleton instance of the Discovery service
+	 */
+	public static Discovery getInstance() {
+		return DiscoveryImpl.getInstance();
+	}
+}
+
+/**
+ * Implementation of the multicast discovery service
+ */
+class DiscoveryImpl implements Discovery {
+	
+	private static Logger Log = Logger.getLogger(Discovery.class.getName());
+
+	// The pre-aggreed multicast endpoint assigned to perform discovery.
+
+	static final int DISCOVERY_RETRY_TIMEOUT = 5000;
+	static final int DISCOVERY_ANNOUNCE_PERIOD = 1000;
+
+	// Replace with appropriate values...
+	static final InetSocketAddress DISCOVERY_ADDR = new InetSocketAddress("226.226.226.226", 2266);
+
+	// Used separate the two fields that make up a service announcement.
+	private static final String DELIMITER = "\t";
+
+	private static final int MAX_DATAGRAM_SIZE = 65536;
+
+	private static Discovery singleton;
+
+	Map <String,List<URI>> servicesUris = new HashMap<>();
+
+	synchronized static Discovery getInstance() {
+		if (singleton == null) {
+			singleton = new DiscoveryImpl();
+		}
+		return singleton;
+	}
+
+	private DiscoveryImpl() {
+		this.startListener();
+	}
+
+	@Override
+	public void announce(String serviceName, String serviceURI) {
+        Log.info(String.format("Starting Discovery announcements on: %s for: %s -> %s\n", DISCOVERY_ADDR, serviceName,
+				serviceURI));
+
+            var pktBytes = String.format("%s%s%s", serviceName, DELIMITER, serviceURI).getBytes();
+            var pkt = new DatagramPacket(pktBytes, pktBytes.length, DISCOVERY_ADDR);
+
+		// start thread to send periodic announcements
+		new Thread(() -> {
+			try (var ds = new DatagramSocket()) {
+				while (true) {
+					try {
+						ds.send(pkt);
+						Thread.sleep(DISCOVERY_ANNOUNCE_PERIOD);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+	}
+
+
+	@Override
+	public URI[] knownUrisOf(String serviceName, int minEntries) {
+		List<URI> uriList;
+		while(true){
+			uriList = servicesUris.get(serviceName);
+			if(uriList != null && uriList.size() >= minEntries){
+				break;
+			}
+		}
+		return uriList.toArray(new URI[uriList.size()]);
+	}
+
+	private void startListener() {
+        Log.info(String.format("Starting discovery on multicast group: %s, port: %d\n", DISCOVERY_ADDR.getAddress(),
+				DISCOVERY_ADDR.getPort()));
+
+		new Thread(() -> {
+			try (var ms = new MulticastSocket(DISCOVERY_ADDR.getPort())) {
+				ms.joinGroup(DISCOVERY_ADDR, NetworkInterface.getByInetAddress(InetAddress.getLocalHost()));
+				for (;;) {
+					try {
+						var pkt = new DatagramPacket(new byte[MAX_DATAGRAM_SIZE], MAX_DATAGRAM_SIZE);
+						ms.receive(pkt);
+
+						var msg = new String(pkt.getData(), 0, pkt.getLength());
+
+						var parts = msg.split(DELIMITER);
+						if (parts.length == 2) {
+							var serviceName = parts[1];
+							var uri = URI.create(parts[0]);
+                            addToServicesMap(serviceName, uri);
+						}
+
+					} catch (Exception x) {
+						x.printStackTrace();
+					}
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		}).start();
+	}
+	/**
+	 * Adds a URI to the map
+	 * @param serviceName
+	 * @param uri
+	 */
+	private void addToServicesMap(String serviceName, URI uri) {
+		List<URI> serviceUris = servicesUris.get(serviceName);
+		if(serviceUris == null){
+			serviceUris = new ArrayList<>();
+		}
+		serviceUris.add(uri);
+		servicesUris.put(serviceName, serviceUris);
+	}
+}
